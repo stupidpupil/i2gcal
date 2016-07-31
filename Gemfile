f2be1e446d27232591e7caada5bc2cfb2c7fbbbe
Adam Watkins <adam@stupidpupil.co.uk>
2016-07-31 11:55:01 +0100

    Add coverage

--- Gemfile
+++ Gemfile
@@ -8,3 +8,11 @@ gem 'uuidtools', '~> 2.1'
 group :development do
   gem 'bacon', '~> 1.2'
 end
+
+group :test do
+  if ENV['TRAVIS']
+    gem 'codeclimate-test-reporter', require: false
+  else
+    gem 'simplecov', require: false
+  end
+end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,3 +1,11 @@
+if ENV['TRAVIS']
+  require 'codeclimate-test-reporter'
+  CodeClimate::TestReporter.start
+else
+  require 'simplecov'
+  SimpleCov.start
+end
+
 require 'synchrograph'
 
 FIXTURES_DIR = File.expand_path('../fixtures/', __FILE__)
