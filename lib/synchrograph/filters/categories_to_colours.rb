6ee41af17ceaf84c9869664d943ee2d7da134981
Adam Watkins <adam@stupidpupil.co.uk>
2016-10-12 19:10:19 +0100

    Fix issue with multiple categories

--- lib/synchrograph/filters/categories_to_colours.rb
+++ lib/synchrograph/filters/categories_to_colours.rb
@@ -6,7 +6,7 @@ class CatToColFilter
 
   def call(ical_event, representation)
     @mapping.each_pair do |cat, col|
-      representation['colorId'] = col.to_s if ical_event.categories.any? {|c| c.to_sym == cat}
+      representation['colorId'] = col.to_s if ical_event.categories.flatten.any? {|c| c.to_sym == cat}
     end
     representation
   end
